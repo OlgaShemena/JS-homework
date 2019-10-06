c933a96a358e55e020a68d9dd32b2e39ae2ed325
OlgaShemena <52980414+OlgaShemena@users.noreply.github.com>
2019-10-06 16:19:38 +0300

    Create homework_1.js

---
+++ homework_1.js
@@ -0,0 +1,19 @@
+1 let fruit, b
+b="fgdfjh"/2
+fruit="ba"+b+"a"
+"baNaNa"
+
+2 let breakfast
+breakfast= fruit
+
+3 const price=3.14; const dol="USD"
+console.log(price+dol);
+
+4 let a, u
+a=2
+u=6
+let k
+k=a**u;
+
+5 let firstProg
+firstProg='Hello world!';
